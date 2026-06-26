34a463071ec153dc16ee024b9cb8fd1cecf9a2ae
Yash Chhalotre <yashchhalotre400@email.com>
2026-06-26 21:48:24 +0530

    Update CORS for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,10 @@ const app = express()
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'http://localhost:5173',  // your React app's URL
-  credentials: true,                // needed for cookies (your token cookie)
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json())
